c450568354b73fce046523be738f67e4c0128c1c
Chandan Bhandari <chandan@deepcellbio.com>
2023-12-27 13:43:05 +0530

    initial commit

---
+++ projects/main.go
@@ -0,0 +1,8 @@
+package main // package declaration
+
+import "fmt" // import other packages that we need
+
+func main() { // declare functions. tell go to do things
+	fmt.Println("Hello, world!")
+}
+
