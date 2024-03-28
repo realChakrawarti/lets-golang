a30cc36ea284a351cbc3be29be735f065e86e064
realChakrawarti <anupam.chakrawarti@outlook.com>
2024-03-28 13:02:16 +0530

    refactor investment CLI app

--- investment/investment_calculator.go
+++ investment/investment_calculator.go
@@ -6,24 +6,22 @@ import (
 )
 
 func main() {
-	var investmentAmount, years, inflationRate float64
-	expectedReturnRate := 5.5
-
-	fmt.Print("Investment Amount: ")
-	fmt.Scan(&investmentAmount)
-
-	fmt.Print("Expected Return Rate: ")
-	fmt.Scan(&expectedReturnRate)
-
-	fmt.Print("Years: ")
-	fmt.Scan(&years)
-
-	fmt.Print("Inflation Rate (enter 0 to ignore inflation): ")
-	fmt.Scan(&inflationRate)
+	investmentAmount := takeInput("Investment Amount: ")
+	expectedReturnRate := takeInput("Expected Return Rate: ")
+	years := takeInput("Years: ")
+	inflationRate := takeInput("Inflation Rate (enter 0 to ignore inflation): ")
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	inflationAdjustedFutureValue := futureValue * math.Pow(1+inflationRate/100, years)
 
-	fmt.Println("Amount on maturity: ", futureValue)
-	fmt.Println("Inflation adjusted: ", inflationAdjustedFutureValue)
+	fmt.Printf("Amount on maturity: %0.2f\n", futureValue)
+	fmt.Printf("Inflation adjusted: %0.2f\n", inflationAdjustedFutureValue)
+}
+
+func takeInput(label string) float64 {
+	var inputValue float64
+	fmt.Print(label)
+	fmt.Scan(&inputValue)
+
+	return inputValue
 }
