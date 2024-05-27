e7318a79c6f0aa37a99147478decfee6c81cc0fc
realChakrawarti <anupam.chakrawarti@outlook.com>
2024-05-28 01:19:28 +0530

    project(rest-event): add GET & POST event request

--- projects/rest-event/main.go
+++ projects/rest-event/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"realChakrawarti/rest-event/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,12 +12,34 @@ func main() {
 
 	// Setup handler, GET, POST, PUT, PATCH, DELETE
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":42069") // localhost:42069
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "Hello!",
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Couldn't parse request data",
+		})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "Event created",
+		"data":    event,
 	})
 }
