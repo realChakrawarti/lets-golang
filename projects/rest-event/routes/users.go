75e489e19ed3aa3cbd0e45e4c44a283b1d38102b
realChakrawarti <anupam.chakrawarti@outlook.com>
2024-06-01 23:34:35 +0530

    project(rest-event): add foreign key for user and POST signup

---
+++ projects/rest-event/routes/users.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"net/http"
+	"realChakrawarti/rest-event/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Couldn't parse request data",
+		})
+		return
+	}
+
+	err = user.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not save user. Please try again after sometime",
+		})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "User created successfully!",
+	})
+}
