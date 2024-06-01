289587d1a605fb29ecb7e827256c0712b11374af
realChakrawarti <anupam.chakrawarti@outlook.com>
2024-06-01 23:02:08 +0530

    project(rest-event): DELETE event

--- projects/rest-event/routes/events.go
+++ projects/rest-event/routes/events.go
@@ -119,3 +119,40 @@ func updateEvent(context *gin.Context) {
 		"message": "Event updated successfully!",
 	})
 }
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse the event id.",
+		})
+
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not fetch event. Please try again after sometime!",
+		})
+
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not delete event. Please try again after sometime!",
+		})
+
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Event deleted successfully!",
+	})
+
+}
